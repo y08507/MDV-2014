7bcdbab36a1baca567f45ff62744777a8af2c182
Luis Camacho Jr <LuisCamachoJr@fullsail.edu>
2013-03-02 20:59:51 -0500

    Added Files

---
+++ SDI/SDI Project 3/JSON.js
@@ -0,0 +1,21 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Project31312
+ * Date: 1/24/13
+ * Time: 1:56 PM
+ * To change this template use File | Settings | File Templates.
+ */
+// Author: Luis Camacho Jr
+// Created for: SDI 1301
+// Project 3 Refactored Project 2
+// Flowchart and Objects
+
+//JSON Data
+
+var codeName;
+codeName = {
+    "spartanName":{
+        "name":"GoldenArm"
+    }
+
+};
