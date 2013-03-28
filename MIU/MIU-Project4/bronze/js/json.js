6bef92dd134a079a1d694ff55a2d93fce693aefa
Luis Camacho Jr <LuisCamachoJr@fullsail.edu>
2013-03-28 19:53:42 -0400

    Added Project 4 folder and files Adding Refactored Gold and Bronze in JQM and Theme Roller

---
+++ MIU/MIU-Project4/bronze/js/json.js
@@ -0,0 +1,270 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Project31312
+ * Date: 2/17/13
+ * Time: 10:57 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/** Author: Luis Camacho Jr.
+ *Full Sail University
+ *Course: MIU
+ *Term: 1303
+ *Project 2: Feature, Search & Browse
+ *Implementation of jQuery Mobile
+ */
+
+var json = {
+    "clientData1":{
+        "fname":["First Name:", "Clyde"],
+        "lname":["Last Name:", "Parker"],
+        "ename":["E-Mail Address:", "Clyde@ParkerInc.com"],
+        "pnumber":["Phone Number:", "243-123-4545"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Business Formation"],
+        "date":["Consultation Date:", "2013-03-25"],
+        "payment":["Method of Payment:", "TravelersCheck"],
+        "notes":["Client Comments:", "Trademark and Partnership questions."],
+        "app":["Rate App:", "10"]
+    },
+    "clientData2":{
+        "fname":["First Name:", "John"],
+        "lname":["Last Name:", "Smith"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "747-488-1212"],
+        "status":["Client Status:", "ExistingClient"],
+        "type":["Case Type:", "Immigration Law"],
+        "date":["Consultation Date:", "2013-03-25"],
+        "payment":["Method of Payment:", "Visa"],
+        "notes":["Client Comments:", "Rush Service"],
+        "app":["Rate App:", "6"]
+    },
+    "clientData3":{
+        "fname":["First Name:", "Charles"],
+        "lname":["Last Name:", "Wilson"],
+        "ename":["E-Mail Address:", "cw12@mail.com"],
+        "pnumber":["Phone Number:", "247-488-1213"],
+        "status":["Client Status:", "ExistingClient"],
+        "type":["Case Type:", "Document Drafting"],
+        "date":["Consultation Date:", "2013-04-23"],
+        "payment":["Method of Payment:", "TravelersCheck"],
+        "notes":["Client Comments:", "Confirm with Client"],
+        "app":["Rate App:", "7"]
+    },
+    "clientData4":{
+        "fname":["First Name:", "Jean"],
+        "lname":["Last Name:", "Luke"],
+        "ename":["E-Mail Address:", "jluke@lmail.com"],
+        "pnumber":["Phone Number:", "447-458-6212"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Immigration Law"],
+        "date":["Consultation Date:", "2013-04-23"],
+        "payment":["Method of Payment:", "AmericanExpress"],
+        "notes":["Client Comments:", "Discuss H-1B Visa for Specialty Occupation Professionals"],
+        "app":["Rate App:", "8"]
+    },
+    "clientData5":{
+        "fname":["First Name:", "Clarence"],
+        "lname":["Last Name:", "Rumsey"],
+        "ename":["E-Mail Address:", "clarRum@mine.com"],
+        "pnumber":["Phone Number:", "947-088-2214"],
+        "status":["Client Status:", "ExistingClient"],
+        "type":["Case Type:", "Immigration Law"],
+        "date":["Consultation Date:", "2013-04-23"],
+        "payment":["Method of Payment:", "Cash"],
+        "notes":["Client Comments:", "O-1/P Visa for Artists"],
+        "app":["Rate App:", "9"]
+    },
+    "clientData6":{
+        "fname":["First Name:", "Donald"],
+        "lname":["Last Name:", "Blanc"],
+        "ename":["E-Mail Address:", "Don@hmail.com"],
+        "pnumber":["Phone Number:", "703-222-3202"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Business Formation"],
+        "date":["Consultation Date:", "2013-04-26"],
+        "payment":["Method of Payment:", "Visa"],
+        "notes":["Client Comments:", "Questions on Incorporating"],
+        "app":["Rate App:", "10"]
+    },
+    "clientData7":{
+        "fname":["First Name:", "Rudolf"],
+        "lname":["Last Name:", "Kerk"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "243-387-6654"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Immigration Law"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "Visa"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "6"]
+    },
+    "clientData8":{
+        "fname":["First Name:", "Lawrence"],
+        "lname":["Last Name:", "Fishbur"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "565-456-8723"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Immigration Law"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "AmericanExpress"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "7"]
+    },
+    "clientData9":{
+        "fname":["First Name:", "Travis"],
+        "lname":["Last Name:", "Walki"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "122-687-8888"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Document Drafting"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "Visa"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "8"]
+    },
+    "clientData10":{
+        "fname":["First Name:", "Tim"],
+        "lname":["Last Name:", "Clouie"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "213-233-2323"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Immigration Law"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "Visa"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "10"]
+    },
+    "clientData21":{
+        "fname":["First Name:", "Strek"],
+        "lname":["Last Name:", "Florin"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "127-397-1009"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Immigration Law"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "AmericanExpress"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "10"]
+    },
+    "clientData11":{
+        "fname":["First Name:", "Blanche"],
+        "lname":["Last Name:", "Reeci"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "213-488-4567"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Business Formation"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "Cash"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "8"]
+    },
+    "clientData12":{
+        "fname":["First Name:", "Claud"],
+        "lname":["Last Name:", "Trambor"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "342-488-1212"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Document Drafting"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "Visa"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "9"]
+    },
+    "clientData13":{
+        "fname":["First Name:", "Frank"],
+        "lname":["Last Name:", "Knight"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "232-400-5320"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Immigration Law"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "Visa"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "6"]
+    },
+    "clientData14":{
+        "fname":["First Name:", "Miche"],
+        "lname":["Last Name:", "lu"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "217-476-5432"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Document Drafting"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "Visa"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "10"]
+    },
+    "clientData15":{
+        "fname":["First Name:", "Toni"],
+        "lname":["Last Name:", "Sliche"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "709-488-4321"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Document Drafting"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "Visa"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "10"]
+    },
+    "clientData16":{
+        "fname":["First Name:", "Clark"],
+        "lname":["Last Name:", "Lomack"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "732-488-8934"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Business Formation"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "Visa"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "5"]
+    },
+    "clientData17":{
+        "fname":["First Name:", "Tyri"],
+        "lname":["Last Name:", "Nembro"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "712-488-1230"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Immigration Law"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "AmericanExpress"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "10"]
+    },
+    "clientData18":{
+        "fname":["First Name:", "Chip"],
+        "lname":["Last Name:", "Waters"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "908-488-0009"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Document Drafting"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "Visa"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "3"]
+    },
+    "clientData19":{
+        "fname":["First Name:", "Lucy"],
+        "lname":["Last Name:", "Fona"],
+        "ename":["E-Mail Address:", "Smith@Smith.com"],
+        "pnumber":["Phone Number:", "879-488-3211"],
+        "status":["Client Status:", "NewClient"],
+        "type":["Case Type:", "Immigration Law"],
+        "date":["Consultation Date:", "2013-07-21"],
+        "payment":["Method of Payment:", "Visa"],
+        "notes":["Client Comments:", "Priority Service"],
+        "app":["Rate App:", "10"]
+    },
+    "clientData20":{
+        "fname":["First Name:", "Rick"],
+        "lname":["Last Name:", "Chamborganin"],
+        "ename":["E-Mail Address:", "tommyllc@landl.com"],
+        "pnumber":["Phone Number:", "213-238-9823"],
+        "status":["Client Status:", "ExistingClient"],
+        "type":["Case Type:", "Business Formation"],
+        "date":["Consultation Date:", "2013-04-17"],
+        "payment":["Method of Payment:", "Cash"],
+        "notes":["Client Comments:", "Give me a call This evening if possible"],
+        "app":["Rate App:", "8"]
+    }
+};
---
+++ MIU/MIU-Project4/gold/js/main.js
@@ -0,0 +1,241 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Project31312
+ * Date: 3/21/13
+ * Time: 8:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+/*   Author: Luis Camacho Jr.
+     Full Sail University
+     Course: MIU
+     Term: 1302
+     Project 4 Gold and Bronze: Feature, Search & Browse
+     Implementation of jQuery Mobile and Aesthetic
+*/
+
+
+/*$('#pageOne').on('pageinit', function(){
+    //code needed for home page goes here
+
+});
+*/
+/*var clientManagerStoredData = function(data){
+    console.log(data);
+    clientManagerStoredData(data)
+};*/
+
+
+$('#addItem').on('pageinit', function () {
+    var myForm = $('#clientManagerForm');
+    var clientManagerErrors = $('#clientManagerErrors');
+
+
+    myForm.validate({
+        ignore:".ignore",
+
+        invalidHandler: function (form, validator) {
+
+
+            // As per video this is the code to create dialog for errors continue working on for project 4
+            /* clientManagerErrors.click();
+             var html = '';
+             for(var key in validator.submitted){
+             var label = $('label[for^="'+ key +'"]').not('[generated]');
+
+                 console.log(validator.submitted);
+             var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+            var fieldName = legend.length ? legend.text() : label.text() : label.text();
+             html += '<li>'+ fieldName +'</li>';
+             };
+             $("#errorDialog ul").html(html);*/
+        },
+        submitHandler: function() {
+            var data = myForm.serializeArray();
+            storeData(this.key);
+            location.reload();
+        }
+
+
+
+
+
+
+    });
+    $('#errorDialog1').click(function (){
+        var ask = confirm("Are you sure you want to Reset Client Form?");
+        if(ask){
+
+            alert("Client Form Reset Complete!");
+            location.reload();
+        }else{
+            alert("Client Form Reset Process Canceled.")
+        }
+    });
+    /* Working on advancing code and preparation for next course
+   $('#clientManagerErrors1').click(function (){
+        var ask = confirm("Are you sure you want to Exit Form?");
+        if(ask){
+
+            alert("Exiting Form Returning To Home Page!");
+            location.replace("#pageOne");
+        }else{
+            alert("Client Form Exit Process Canceled.")
+        }
+    });*/
+
+
+
+
+    //any other code needed for addItem page goes here
+    //getElement by ID Function
+
+    function g(e) {
+        var theElement;
+        theElement = document.getElementById(e);
+        return theElement;
+    }
+
+    //Find value of selected radio button.
+
+    function radioSelection() {
+        //noinspection JSUnresolvedVariable
+        var clientStatus = document.getElementById("clientManagerForm").client;
+        for (var i = 0; i < clientStatus.length; i++) {
+            if (clientStatus[i].checked) {
+                status = clientStatus[i].value;
+            }
+        }
+    }
+
+    function checkSelection() {
+        //noinspection JSUnresolvedVariable
+        var checkBoxes = document.getElementById("clientManagerForm").caseType;
+        var checkedBox = [];
+        for (var i = 0; i < checkBoxes.length; i++) {
+            if (checkBoxes[i].checked) {
+                checkedBox.push(checkBoxes[i].value);
+                selectedBox = checkedBox;
+            }
+        }
+        console.log(checkedBox);
+    }
+
+    //Accessing and storing Declared Variables
+    //localStorage is a Key Value pair.
+
+    function storeData(key) {
+        //key creation only if new item, then generate key.
+        if (!key) {
+            var id = Math.floor(Math.random() * 10000002);
+            //retrieve all data form fields value and store in an object.
+            //Object properties contain array with the form label and input value.
+        } else {
+            //Keep the same key value for editing contact item.
+            //Key has been utilized throughout process. Key is from editSubmit event handler.
+            //to validator function and then to function storeData.
+            id = key;
+        }
+        radioSelection();
+        checkSelection();
+        var item = {};
+        item.fname = ["First Name:", g('firstName').value];
+        item.lname = ["Last Name:", g('lastName').value];
+        item.ename = ["E-Mail Address:", g('email').value];
+        item.pnumber = ["Phone Number:", g('phoneNumber').value];
+        item.status = ["Client Status:", status];
+        //item.type    = ["Case Type:", selectedBox];
+        item.date = ["Consultation Date:", g('firstConsult').value];
+        item.payment = ["Method of Payment:", g('payment').value];
+        item.notes = ["Client Comments:", g('clientFeedback').value];
+        //item.app     = ["Rate App:", g('ratingApp').value];
+        //Save data to Local Storage: Use Stringify to convert our object to a string.
+        //localStorage is a Key Value pair.
+        //noinspection JSValidateTypes
+        localStorage.setItem(id, JSON.stringify(item));
+        alert("Saving Data! Coming Soon.. Select Display Data Link & Edit Data Link!");
+        //console.log(selectedBox);
+    }
+    function getData(){
+        //toggleControls("on");
+
+        if (localStorage.length === 0) {
+            alert("There is no Data in Local Storage. Test Data Being Loaded");
+            //testData function utilizes json.js file to populate the form with Data.
+            //For testing purposes.
+            testData();
+        }
+        //Write Data from local storage to the browser.
+        //toggleControls("on");
+        var makeDiv = document.createElement('div');
+        makeDiv.setAttribute("id", "items");
+        var makeList = document.createElement('ul');
+        makeDiv.appendChild(makeList);
+        document.body.appendChild(makeDiv);
+        //g('items').style.border = "block";
+        for (var i = 0, len = localStorage.length; i < len; i++) {
+            var makeli = document.createElement('li');
+            var newLinksLi = document.createElement('li');
+            makeList.appendChild(makeli);
+            var key = localStorage.key(i);
+            var value = localStorage.getItem(key);
+            //convert the string from local storage value back to an object using JSON.parse()
+            var obj;
+            obj = JSON.parse(value);
+            var makeSublist = document.createElement('ul');
+            makeli.appendChild(makeSublist);
+            clientStatus(obj.status[1], makeSublist);
+            for (var n in obj) {
+                var makeSubli = document.createElement('li');
+                makeSublist.appendChild(makeSubli);
+                //noinspection JSUnfilteredForInLoop
+                makeSubli.innerHTML = obj[n][0] + " " + obj[n][1];
+                makeSublist.appendChild(newLinksLi);
+                //g('items').style.display = "block";
+                console.log(key);
+            }
+            //Create our edit and delete buttons
+            makeItemLinks(localStorage.key(i), newLinksLi);
+            //Create horizontal rule to separate items in edit list
+            var separate = document.createElement('hr');
+            separate.setAttribute("id","editItemSeparator");
+            makeSublist.appendChild(separate);
+        }
+    }
+    //Get the image for the right client type
+    function clientStatus(clientType,makeSublist){
+        var imageLi = document.createElement('li');
+        makeSublist.appendChild(imageLi);
+        var newImg = document.createElement('img');
+        newImg.setAttribute("src", "img/"+clientType+".png");
+        imageLi.appendChild(newImg);
+        console.log(clientType);
+    }
+
+
+
+
+});
+
+
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+/*var autofillData = function (){
+
+ };
+
+ var getData = function(){
+
+ };
+
+ var storeData = function(data){
+
+ };
+
+ var	deleteItem = function (){
+
+ };
+
+ var clearLocal = function(){
+
+ };
+ */
